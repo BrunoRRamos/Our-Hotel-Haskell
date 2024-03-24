ee7e53ac5d13c39cf24bde95016cb411e0902180
Bruno <bruno.ramos@ccc.ufcg.edu.br>
2024-03-23 23:38:11 -0300

    feat(Login): Criando função de login

---
+++ src/Login.hs
@@ -0,0 +1,14 @@
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+
+module Login
+    ( login
+    ) where
+
+verifyEmail :: String -> [String] -> Bool
+verifyEmail email (h : t) = (h == email) || verifyEmail email t
+
+verifyPassword :: String -> [String] -> Bool
+verifyPassword password (h : t) = (h == password) || verifyPassword password t
+
+login :: String -> String -> [String] -> Bool
+login email password usersArr = verifyEmail email usersArr && verifyPassword password usersArr
