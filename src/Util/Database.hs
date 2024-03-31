f807d8179e0d18d322aa6e1ff0abf380e8c7ef08
PedroCost4 <pedro.costacdc@gmail.com>
2024-03-31 16:20:37 -0300

    feat: check admin user

--- src/Util/Database.hs
+++ src/Util/Database.hs
@@ -6,14 +6,16 @@ import Database.SQLite.Simple ( Connection, open )
 import Models.Reservation (createReservationTable)
 import Models.Room (createRoomTable)
 import Models.Service (createServiceTable)
-import Models.User (createUserTable, createUser, User (..), Role (ADMIN))
+import Models.User (createUserTable, createUser, User (..), Role (ADMIN), getUser)
+import Control.Monad (when)
+import Data.Maybe (isNothing)
 
 startDb :: IO Connection
 startDb = do
   conn <- open "hotel.db"
   createUserTable conn
   --default admin user
-  createUser conn User {
+  let user = User {
     _email = "admin@admin.com"
   , _firstName = "Admin"
   , _lastName = "Admin"
@@ -22,6 +24,8 @@ startDb = do
   , _block_reason = Nothing
   , _role = ADMIN 
   }
+  userExists <- getUser conn (_email user)
+  when (isNothing userExists) $ createUser conn user
   createReservationTable conn
   createRoomTable conn
   createServiceTable conn
