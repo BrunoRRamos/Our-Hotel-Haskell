0718bb8e44d7fb9bd622c02cf43675135d64d46c
PedroCost4 <pedro.costacdc@gmail.com>
2024-03-31 16:06:52 -0300

    feat: admin user

--- src/Util/Database.hs
+++ src/Util/Database.hs
@@ -6,12 +6,22 @@ import Database.SQLite.Simple ( Connection, open )
 import Models.Reservation (createReservationTable)
 import Models.Room (createRoomTable)
 import Models.Service (createServiceTable)
-import Models.User (createUserTable)
+import Models.User (createUserTable, createUser, User (..), Role (ADMIN))
 
 startDb :: IO Connection
 startDb = do
   conn <- open "hotel.db"
   createUserTable conn
+  --default admin user
+  createUser conn User {
+    _email = "admin@admin.com"
+  , _firstName = "Admin"
+  , _lastName = "Admin"
+  , _password = "admin"
+  , _isActive = True
+  , _block_reason = Nothing
+  , _role = ADMIN 
+  }
   createReservationTable conn
   createRoomTable conn
   createServiceTable conn
